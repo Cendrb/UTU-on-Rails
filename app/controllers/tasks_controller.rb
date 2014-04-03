c2998194ebe2ab269beff98515efe13f93e6f970
Cendrb <cendrb@gmail.com>
2014-04-03 19:01:31 +0200

    [BUGFIX] Additional info in tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,6 +70,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :description, :subject, :date, :group)
+      params.require(:task).permit(:title, :description, :subject, :date, :group, :additional_info_url)
     end
 end
