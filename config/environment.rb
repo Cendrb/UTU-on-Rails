05f7c698d5488f627eed9a01a9226c4a640a7fbb
Cendrb <cendrb@gmail.com>
2014-04-17 11:22:27 +0200

    [FEATURE] Mail configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 UTUOnRails::Application.initialize!
+
+# Mail config
+config.action_mailer.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smatp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: adisinfoapp,
+    password: "suprakindrlo",
+    enable_starttls_auto: true
+  }
+end
