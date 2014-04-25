4856e33c5fa5775f377011dfc7d7c9398fef40e1
Cendrb <cendrb@gmail.com>
2014-04-25 18:45:29 +0200

    [FEATURE] Name for usrs - only migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423114548) do
+ActiveRecord::Schema.define(version: 20140425164216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20140423114548) do
     t.integer  "group"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
