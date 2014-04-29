2b82e6e7e2b8a6797d6670238f34d7ebce256577
Cendrb <cendrb@gmail.com>
2014-04-29 15:55:26 +0200

    [FEATURE] errors_restricted_cs added
    better error localization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def errors_restricted_cs (number_of_errors)
+    if number_of_errors == 1
+      return "1 chyba zabránila"
+    end
+    if number_of_errors <= 4
+      return "#{number_of_errors} chyby zabránily"
+    end
+    if number_of_errors > 4
+      return "#{number_of_errors} chyb zabránilo"
+    end
+  end
 end
