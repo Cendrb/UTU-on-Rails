19254893b8f1753da31316bebfa21c801fd0dd55
Cendrb <cendrb@gmail.com>
2014-05-03 16:27:03 +0200

    [FEATURE] Mobile basics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,23 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+  
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
+  end
+  
+  def mobile_device?
+    if sesion[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app' + 'views_mobile'
+  end
 
   protected
 
@@ -55,5 +72,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  helper_method :logged_in?, :current_user, :admin_logged_in?
+  helper_method :logged_in?, :current_user, :admin_logged_in?, :mobile_device?
 end
