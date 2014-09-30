45ce07daf666a611af6429886de05433671994ba
Cendrb <cendrb@gmail.com>
2014-09-30 21:24:45 +0200

    probably working

--- app/controllers/day_bitches_controller.rb
+++ app/controllers/day_bitches_controller.rb
@@ -2,6 +2,7 @@ class DayBitchesController < ApplicationController
   before_filter :authenticate_admin, except: [:new, :create]
   before_filter :authenticate, only: [:new, :create]
   before_action :set_day_bitch, only: [:show, :edit, :update, :destroy]
+  before_action :set_bitches_list_variable, only: [:edit, :new, :create]
   # GET /day_bitches
   # GET /day_bitches.json
   def index
@@ -12,16 +13,19 @@ class DayBitchesController < ApplicationController
   # GET /day_bitches/1.json
   def show
   end
+  
+  def thanks
+    
+  end
 
   # GET /day_bitches/new
   def new
-    set_bitches_list_variable
     @day_bitch = DayBitch.new
   end
 
   # GET /day_bitches/1/edit
   def edit
-    set_bitches_list_variable
+
   end
 
   # POST /day_bitches
@@ -30,15 +34,18 @@ class DayBitchesController < ApplicationController
     @day_bitch = DayBitch.new(day_bitch_params)
     @day_bitch.date = Date.today
     @day_bitch.user = current_user
+    
+    today_user_bitches = DayBitch.where("date = :today AND user_id = :user", { today: Date.today, user: current_user } )
 
-    respond_to do |format|
-      if @day_bitch.save
-        format.html { redirect_to @day_bitch, notice: 'Day bitch was successfully created.' }
-        format.json { render :show, status: :created, location: @day_bitch }
-      else
-        format.html { render :root }
-        format.json { render json: @day_bitch.errors, status: :unprocessable_entity }
-      end
+    if(today_user_bitches.count > 0)
+      redirect_to :vote, alert: 'Můžete hlasovat pouze jednou denně.'
+      return
+    end
+
+    if @day_bitch.save
+      redirect_to :vote, notice: 'Váš hlas byl zaznamenán.'
+    else
+      redirect_to :root, alert: "Váš hlas se nepodařilo uložit."
     end
   end
 
