12727d72721b1a985c6c16d7653b80564048219c
Cendrb <cendrb@gmail.com>
2014-10-27 21:57:05 +0100

    Validations

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -7,7 +7,7 @@ class Exam < ActiveRecord::Base
   scope :id_not_on_list, lambda { |list| where("NOT (ARRAY[id] <@ ARRAY[:ids])", { ids: list } ) }
   scope :id_on_list, lambda { |list| where("ARRAY[id] <@ ARRAY[:ids]", { ids: list } ) }
   
-  validates :title, :group, :date, presence: {presence: true, message: "nesmí být prázdný"}
+  validates :title, :subject_id, :group, :date, presence: {presence: true, message: "nesmí být prázdný"}
   validates :group, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 2, only_integer: true, message: "neexistuje - zadejte 0 pro obě skupiny" }
   
   def get_task
