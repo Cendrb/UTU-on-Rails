2aa28930c1a56efa8e632710ee10def3d769d0b7
Cendrb <cendrb@gmail.com>
2014-10-28 19:12:26 +0100

    allow index to everyone

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_admin
+  before_filter :authenticate_admin, except: [:index, :show]
 
   # GET /subjects
   # GET /subjects.json
