cf593d38016cfe61dce7f0eecf4c57eea1c1d596
Cendrb <cendrb@gmail.com>
2015-01-06 22:39:15 +0100

    more password forgot

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,17 @@
 require 'securerandom'
 
 class UserMailer < ActionMailer::Base
-  default from: "UTU"
-  
+  default from: "utu@utu.com"
   def forgot_password(user)
     code = SecureRandom.hex
     user.forgot_password_code = code
     user.save!
     @user = user
-    @url = "http://localhost:3000/forgot_code?code=#{code}"
+    if Rails.env.development?
+      @url = "http://localhost:3000/forgot_code?code=#{code}"
+    else
+      @url = "http://utu.herokuapp.com/forgot_code?code=#{code}"
+    end
     mail(to: user.email, subject: "Změna hesla")
   end
 end
