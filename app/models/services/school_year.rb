20525d5cf6f9c15214404e34f12b9da19dc543a4
Čeněk Drbal <cendrb@gmail.com>
2016-01-11 22:03:07 +0100

    yreasr whit

--- app/models/services/school_year.rb
+++ app/models/services/school_year.rb
@@ -5,4 +5,11 @@ class SchoolYear < ActiveRecord::Base
   def readable_name
     return "#{beginning_year}/#{beginning_year + 1}"
   end
+
+  def generate_services_from(date)
+    if date >= Date.new(beginning_year, 9, 1) && date <= Date.new(beginning_year + 1, 6, 30)
+      # date is in current school year
+
+    end
+  end
 end
