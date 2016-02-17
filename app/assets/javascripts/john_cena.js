dcfb21a35048de092813c7be6698dbc0b765bbb6
Čeněk Drbal <cendrb@gmail.com>
2016-02-17 16:55:00 +0100

    [TASK]ŽRRRSA

--- app/assets/javascripts/john_cena.js
+++ app/assets/javascripts/john_cena.js
@@ -1,34 +1,159 @@
-$(function () {
-    var material_container = $("#material_container");
-
-    var base_height = window.innerHeight;
-    var base_width = window.innerWidth;
-
-    // john cena when "kana"
-    var john_cena = new Audio('http://adis.g6.cz/johncena/john-cena.mp3');
-    var maximum_index = 3;
-    var pattern = [107, 97, 110, 97];
-    var current_index = 0;
-    var john_cena_string = "<img src=\"http://adis.g6.cz/johncena/john-cena.jpg\" width=\"1920\" height=\"1080\" class=\"john_cena\">";
-    var john_cena_summoned = false;
-    $(document).keypress(function (e) {
-        if (e.which == pattern[current_index]) {
-            current_index = current_index + 1;
-            if (current_index > maximum_index) {
-                john_cena.cloneNode(true).play();
-                if (!john_cena_summoned) {
-                    $("body").append(john_cena_string);
-                    john_cena_summoned = true;
+KeyboardPhraseDetector = (function () {
+    function KeyboardPhraseDetector() {
+        $(document).keydown(function (e) {
+            var i, len, phraseActionPair, ref, results;
+            ref = KeyboardPhraseDetector.phrasesActionsArray;
+            results = [];
+            console.log(e.which);
+            for (i = 0, len = ref.length; i < len; i++) {
+                phraseActionPair = ref[i];
+                console.log(phraseActionPair.keycodes);
+                console.log(e.which == phraseActionPair.keycodes[phraseActionPair.current_index]);
+                if (e.which == phraseActionPair.keycodes[phraseActionPair.current_index]) {
+                    phraseActionPair.current_index += 1;
+                } else {
+                    phraseActionPair.current_index = 0;
                 }
-                setTimeout(function () {
-                    $(".john_cena").show();
-                    $("#material_container").hide();
-                }, 800);
+                if (phraseActionPair.current_index > (phraseActionPair.keycodes.length - 1)) {
+                    results.push(phraseActionPair.action());
+                } else {
+                    results.push(void 0);
+                }
+            }
+            return results;
+        });
+    }
+
+    KeyboardPhraseDetector.phrasesActionsArray = [];
+
+    KeyboardPhraseDetector.current_index = 0;
+
+    KeyboardPhraseDetector.characters = {
+        a: 65,
+        b: 66,
+        c: 67,
+        d: 68,
+        e: 69,
+        f: 70,
+        g: 71,
+        h: 72,
+        i: 73,
+        j: 74,
+        k: 75,
+        l: 76,
+        m: 77,
+        n: 78,
+        o: 79,
+        p: 80,
+        q: 81,
+        r: 82,
+        s: 83,
+        t: 84,
+        u: 85,
+        v: 86,
+        w: 87,
+        x: 88,
+        y: 89,
+        z: 90
+    };
+
+    KeyboardPhraseDetector.prototype.addListener = function (charactersArray, action) {
+        var character, i, index_sucks, keycodes_array, len;
+        keycodes_array = [];
+        for (index_sucks = i = 0, len = charactersArray.length; i < len; index_sucks = ++i) {
+            character = charactersArray[index_sucks];
+            if (KeyboardPhraseDetector.characters[character] != undefined) {
+                keycodes_array[index_sucks] = KeyboardPhraseDetector.characters[character];
+            } else {
+                keycodes_array[index_sucks] = character;
             }
         }
-        else
-            current_index = 0;
-    });
+        KeyboardPhraseDetector.phrasesActionsArray[KeyboardPhraseDetector.current_index] = {
+            keycodes: keycodes_array,
+            action: action,
+            current_index: 0
+        };
+        return KeyboardPhraseDetector.current_index += 1;
+    };
+
+    return KeyboardPhraseDetector;
+
+})();
+
+setPersistentCookie = function (name, value) {
+    var expiration_date = new Date();
+    var cookie_string = '';
+    expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+    cookie_string = name + "=" + value + "; path=/; expires=" + expiration_date.toGMTString();
+    document.cookie = cookie_string;
+};
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+var zrsa_audio = new Audio('http://adis.g6.cz/johncena/zrsa.mp3');
+var hehou_audio = new Audio('http://adis.g6.cz/johncena/hehou.mp3');
+var john_cena_audio = new Audio('http://adis.g6.cz/johncena/john-cena.mp3');
+var john_cena_string = "<img src=\"http://adis.g6.cz/johncena/john-cena.jpg\" width=\"1920\" height=\"1080\" class=\"john_cena\">";
+var material_container = $("#material_container");
+var john_cena_summoned = false;
+
+function playZrsa(e) {
+    zrsa_copy = zrsa_audio.cloneNode(true);
+    zrsa_copy.volume = 0.6;
+    zrsa_copy.play();
+}
+
+function playHehou(e) {
+    hehou_copy = hehou_audio.cloneNode(true);
+    hehou_copy.volume = 0.8;
+    hehou_copy.play();
+}
+
+function setZrsaListener() {
+    $(document).keydown(playZrsa);
+    $(document).click(playHehou);
+}
+
+var keyboardPhraseDetector;
+keyboardPhraseDetector = new KeyboardPhraseDetector();
+keyboardPhraseDetector.addListener(['k', 'a', 'n', 'a'], function () {
+    john_cena_audio.cloneNode(true).play();
+    if (!john_cena_summoned) {
+        $("body").append(john_cena_string);
+        john_cena_summoned = true;
+    }
+    setTimeout(function () {
+        $(".john_cena").show();
+        material_container.hide();
+    }, 750);
+});
+keyboardPhraseDetector.addListener([20, 20, 20, 'z', 'r', 's', 'a', 20, 20, 20], function () {
+    if (getCookie('zrsa') == 'true') {
+        setPersistentCookie('zrsa', false);
+        alert('ZRSA DISABLED');
+    }
+    else {
+        setPersistentCookie('zrsa', true);
+        setZrsaListener();
+        alert('ZRSA ENABLED');
+    }
+});
+
+if (getCookie('zrsa') == 'true')
+    setZrsaListener();
+
+$(document).ready(function () {
+    var base_height = window.innerHeight;
+    var base_width = window.innerWidth;
 
     // kej sound when "a" clicked - experimental must be enabled
     if (material_container.data("experimental")) {
