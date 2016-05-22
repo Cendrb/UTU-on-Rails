f75ce7e4bd58536c1664ce648022c21603636549
Čeněk Drbal <cendrb@gmail.com>
2016-05-22 22:57:49 +0200

    [TASK] setting config to heroku defaults

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,10 @@ threads threads_count, threads_count
 
 preload_app!
 
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'production'
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
