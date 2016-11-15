cae0d8e25116a3e20bba4e228084ca476d5c0ca3
Cendrb <cendbr@gmail.com>
2016-11-15 19:13:28 +0100

    Add a better seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,95 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-events = [
-  {
-    title: "Představení ab Ivona",
-     description: "Představení o správné soudružce budovatelce Ivoně. Varování: představení trvá cca 23 hodin => spacáky s sebou", 
-     location: "Divadlo Rododendron", 
-     event_start: "10.9.2015", 
-     event_end: "11.9.2015", 
-     pay_date: "1.9.2015", 
-     price: 696
-     },
-     {
-    title: "Představení Pyj na scéně",
-     description: "Krátké představení. V hlavní roli Penis.", 
-     location: "Biograf Modřany", 
-     event_start: "10.9.2016", 
-     event_end: "11.9.2016", 
-     pay_date: "11.9.2016", 
-     additional_info_url: "http://lmgtfy.com"
-     } 
-]
+sclass = Sclass.create!(name: "septima", default_timetable_id: 1)
+class_members = sclass.class_members.create!([{first_name: "Kana", last_name: "Lukas"},
+                                    {first_name: "John", last_name: "Cena"},
+                                    {first_name: "Walter", last_name: "White"},
+                                    {first_name: "Yvonne", last_name: "Uhuh"},
+                                    {first_name: "Stuff", last_name: "Unseen"},
+                                    {first_name: "Great", last_name: "Nothing"},
+                                    {first_name: "Buchen", last_name: "Aš"},
+                                    {first_name: "Lord", last_name: "Crystalis"},
+                                    {first_name: "Sange", last_name: "Yasha"}])
 
-tasks = [
-  {
-    title: "Zabít Andršovou",
-     description: "Co dodat?!", 
-     date: "22.2.2222",
-     subject: 'NJ',
-     group: 2
-     },
-     {
-    title: "Odstřelit pomník IS",
-     description: "69 TNT snad bude stačit :)", 
-     subject: 'MA',
-     date: "23.3.2069",
-     group: 0,
-     additional_info_url: "http://penis.com"
-     } 
-]
+baka_account = BakaAccount.create!(username: "", password: "", class_member: class_members.first)
+timetable = Timetable.create!(name: "the great rozvrh", baka_account: baka_account, sclass: sclass)
 
-exams = [
-  {
-    title: "Rake test",
-     description: "O kurwa", 
-     date: "11.11.2211",
-     subject: 'CH',
-     group: 2
-     },
-     {
-    title: "Maturita",
-     description: "Hurry up!", 
-     subject: 'AJ',
-     date: "23.3.2069",
-     group: 1,
-     additional_info_url: "http://helpforenglish.com"
-     },
-     {
-     title: "Buchbuchhure test",
-     description: "69 TNT snad bude stačit :)", 
-     subject: 'D',
-     date: "23.3.22323",
-     group: 0,
-     } 
-]
 
-services = [
-  {
-    first_name: "Mária Halbrštátová",
-    second_name: "Ivona Součková",
-    service_start: "23.2.2069",
-    service_end: "30.9.2069"
-  },
-  {
-    first_name: "Irina Kobylková",
-    second_name: "Libuše Sakalová",
-    service_start: "23.2.2023",
-    service_end: "30.9.2023"
-  }
-]
+group_category = GroupCategory.create!(name: "hlavní")
+sgroups = group_category.sgroups.create!([{name: "první", timetable_abbr: "1"},{name: "druhá", timetable_abbr: "2"}])
 
-events.each do |params|
-  Event.create(params)
-end
-tasks.each do |params|
-  Task.create(params)
-end
-exams.each do |params|
-  Exam.create(params)
-end
-services.each do |params|
-  Service.create(params)
-end
-User.create(name: "Halbrštátová", email: "penis@gmail.com", password: "penis", password_confirmation: "penis", admin: true, group: 2)
+school_year = SchoolYear.create!(beginning_year: 2016)
+school_year.holidays.create!([{name: "Podzimní prázdniny", holiday_beginning: "2016-10-26", holiday_end: "2016-10-27"},
+                {name: "Vánoční prázdniny", holiday_beginning: "2016-12-23", holiday_end: "2017-01-02"},
+                {name: "Pololetní prázdniny", holiday_beginning: "2017-02-03", holiday_end: "2017-02-03"},
+                {name: "Jarní prázdniny", holiday_beginning: "2017-03-13", holiday_end: "2017-03-19"},
+                {name: "Velikonoční prázdniny", holiday_beginning: "2017-04-13", holiday_end: "2017-04-14"},
+                {name: "Školní výlety", holiday_beginning: "2017-06-21", holiday_end: "2017-06-23"}])
+
+GroupTimetableBinding.create!(timetable: timetable, sgroup: sgroups.last)
+GroupBelonging.create!(class_member: class_members.first, sgroup: sgroups.last)
+User.create!(class_member: class_members.first, email: "penis@gmail.com", password: "penis", password_confirmation: "penis", access_level: User.al_superuser)
