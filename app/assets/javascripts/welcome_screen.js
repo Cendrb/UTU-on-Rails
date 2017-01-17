b730bc3ab1b419885a0bc3dc163b94ad70a788bb
Cendrb <cendrb@gmail.com>
2017-01-17 08:38:50 +0100

    Fix welcome_screen dialog hiding bugs

--- app/assets/javascripts/welcome_screen.js
+++ app/assets/javascripts/welcome_screen.js
@@ -1,4 +1,4 @@
-var mode = "dick"
+var mode = "dick";
 
 $(document).on("turbolinks:load", function () {
     $("#sclass_select").change(function () {
@@ -8,8 +8,6 @@ $(document).on("turbolinks:load", function () {
         else
             $("#welcome_screen_class_saved_section").hide();
 
-        if (mode == "register")
-            $("#welcome_screen_form_placeholder").html("<p>Načítání...</p>");
         $.ajax({
             type: "POST",
             url: "/sclasses/change_current",
@@ -29,37 +27,37 @@ $(document).on("turbolinks:load", function () {
         fetchRegisterForm();
     });
     $(".welcome_screen_login_button").click(function () {
+        mode = "login";
         $("#welcome_screen_form_heading").html("Přihlášení");
         $("#welcome_screen_form_container").show();
-        mode = "login";
         fetchLoginForm();
     });
+
+    function fetchRegisterForm() {
+        $("#welcome_screen_form_placeholder").html("<p>Načítání...</p>");
+        $.ajax({
+            type: "GET",
+            url: "/users/new",
+            data: {target: "#welcome_screen_form_placeholder"},
+            dataType: 'script',
+            format: 'js',
+            error: function () {
+                $("#welcome_screen_form_placeholder").html("<p>Došlo k chybě</p>");
+            }
+        });
+    }
+
+    function fetchLoginForm() {
+        $("#welcome_screen_form_placeholder").html("<p>Načítání...</p>");
+        $.ajax({
+            type: "GET",
+            url: "/login",
+            data: {target: "#welcome_screen_form_placeholder"},
+            dataType: 'script',
+            format: 'js',
+            error: function () {
+                $("#welcome_screen_form_placeholder").html("<p>Došlo k chybě</p>");
+            }
+        });
+    }
 });
-
-function fetchRegisterForm() {
-    $("#welcome_screen_form_placeholder").html("<p>Načítání...</p>");
-    $.ajax({
-        type: "GET",
-        url: "/users/new",
-        data: {target: "#welcome_screen_form_placeholder"},
-        dataType: 'script',
-        format: 'js',
-        error: function () {
-            $("#welcome_screen_form_placeholder").html("<p>Došlo k chybě</p>");
-        }
-    });
-}
-
-function fetchLoginForm() {
-    $("#welcome_screen_form_placeholder").html("<p>Načítání...</p>");
-    $.ajax({
-        type: "GET",
-        url: "/login",
-        data: {target: "#welcome_screen_form_placeholder"},
-        dataType: 'script',
-        format: 'js',
-        error: function () {
-            $("#welcome_screen_form_placeholder").html("<p>Došlo k chybě</p>");
-        }
-    });
-}
